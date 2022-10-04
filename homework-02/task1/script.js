213ce41a4ad41541c80c21a8287da9fa2ce950ac
NataliSkidanenko <sni.nata102@gmail.com>
2022-10-04 16:37:16 +0300

    task-1

---
+++ homework-02/task1/script.js
@@ -0,0 +1,6 @@
+let firstString = prompt('Введіть перший рядок:');
+let secondString = prompt('Введіть другий рядок:');
+let thirdString = prompt('Введіть третій рядок:');
+
+let newSentence = firstString + secondString +thirdString;
+console.log(newSentence);
