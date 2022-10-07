33578aafb59b2f4c9bd864394329f82e59c71089
NataliSkidanenko <sni.nata102@gmail.com>
2022-10-07 14:52:54 +0300

    Moved the declaration of avg

--- homework-06/script.js
+++ homework-06/script.js
@@ -1,12 +1,11 @@
 let firstNum = + prompt('Enter the first number:');
 let secondNum = + prompt('Enter the second number:');
 let thirdNum = + prompt('Enter the third number:');
-let avg;
 
 if(isNaN(firstNum) || isNaN(secondNum) || isNaN(thirdNum)) {
     alert('You entered NOT a number. Try again.');
 } else {
-    avg = (firstNum + secondNum + thirdNum)/3;
+    let avg = (firstNum + secondNum + thirdNum)/3;
     alert(`AVG for ${firstNum}, ${secondNum}, ${thirdNum} = ${avg}`);
 }
 
