3c8c6365faad4969725a9cb192353269f425c854
NataliSkidanenko <sni.nata102@gmail.com>
2022-10-16 20:12:28 +0300

    Considered corner cases

--- homework-13/script.js
+++ homework-13/script.js
@@ -1,9 +1,48 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
-console.log(`Початковий массив: ${array}`);
+const array = [1, false, 3, 4, 5, true, 7, 'Таня', 1];
+alert(`Початковий массив: ${array}`);
+const delElement = convertValue(
+    prompt('Введіть елемент, який потрібно видалити:')
+);
 
-removeElement(array, 5);
-console.log(`Видалено 5 з масиву: ${array}`);
+removeElement(array, delElement);
+alert(`З масива видалено елемент ${delElement}: ${array}`);
+
+function convertValue(value) {
+    if (value.match(/^\d+$/)) {
+        return Number(value);
+    } else if (value === 'true') {
+        return true;
+    } else if (value === 'false') {
+        return false;
+    } else {
+        return value;
+    }
+}
 
 function removeElement(array, item) {
-    array.splice(array.indexOf(item), 1);
+    if (array.includes(item)) {
+        if (array.indexOf(item) === array.lastIndexOf(item)) {
+            removeOneElement(array, item);
+        } else if (array.indexOf(item) !== array.lastIndexOf(item)) {
+            const reply = confirm('Видалити всі елементи з таким значенням?');
+            if (reply === true) {
+                removeAllElements(array, item);
+            } else {
+                removeOneElement(array, item);
+            }
+        }
+    } else {
+        alert('В масиві такого елементу немає!');
+    }
+
+    function removeOneElement(array, item) {
+        array.splice(array.indexOf(item), 1);
+    }
+    function removeAllElements(array, item) {
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] === item) {
+                array.splice(i, 1);
+            }
+        }
+    }
 }
