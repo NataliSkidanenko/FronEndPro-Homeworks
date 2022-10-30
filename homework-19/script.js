4efffb61a8792aaef5b40949618c2e04696ed52a
NataliSkidanenko <sni.nata102@gmail.com>
2022-10-30 20:04:34 +0200

    Fixed bugs

--- homework-19/script.js
+++ homework-19/script.js
@@ -3,12 +3,13 @@ class ApartamentBuilding {
         this.adress = {sreet: street, number: number};
         this.floorsNumber = floorsNumber;
         this.apartamentsNumber = apartamentsNumber;
-        this.apartamentsArray = [0];
+        this.apartamentsArray = [];
         this.dwellersNumber = dwellersNumber;
     }
 
-    addApartamentInfo(apartament) {
+    addApartament(apartament) {
         if (apartament.number > this.apartamentsNumber) {
+            console.log('Квартира з таким номером не може існувати');
             return;
         } else {
             this.apartamentsArray[apartament.number] = apartament;
@@ -17,9 +18,16 @@ class ApartamentBuilding {
 
     getApartament(number) {
         if (number > this.apartamentsNumber) {
-            return `Такої квартири не існує`;
+            console.log('Такої квартири не існує');
+            return;
         }
-        return this.apartamentsArray[number] ?? `Інфо про цю квартиру ще немає`;
+
+        if (this.apartamentsArray[number] === undefined) {
+            console.log('Інфо про цю квартиру ще немає');
+            return;
+        }
+
+        return this.apartamentsArray[number];
     }
 }
 
@@ -32,7 +40,7 @@ class Apartament {
         this.dwellersArray = [];
     }
 
-    addDwellerInfo(dweller) {
+    addDweller(dweller) {
         this.dwellersArray.push(dweller);
     }
 
@@ -44,39 +52,45 @@ class Apartament {
 class Dweller {
     constructor(firstName, lastName, birthDate, phone) {
         this.name = {firstName: firstName, lastName: lastName};
-        this.birthDate = birthDate;
+        this.birthDate = new Date(birthDate);
         this.phone = phone;
     }
 }
 
 const greenville = new ApartamentBuilding('Gmyri', 9, 12, 276, 604);
 
-greenville.addApartamentInfo(new Apartament(7, 65, 48, 3));
-greenville.addApartamentInfo(new Apartament(9, 54, 37, 1));
-greenville.addApartamentInfo(new Apartament(12, 55, 37, 1));
-greenville.addApartamentInfo(new Apartament(12, 202, 62, 3));
-greenville.addApartamentInfo(new Apartament(56, 202, 62, 3));
+greenville.addApartament(new Apartament(7, 65, 48, 3));
+greenville.addApartament(new Apartament(9, 54, 37, 1));
+greenville.addApartament(new Apartament(12, 55, 37, 1));
+greenville.addApartament(new Apartament(12, 202, 62, 3));
+greenville.addApartament(new Apartament(56, 202, 62, 3));
 
 greenville
     .getApartament(65)
-    .addDwellerInfo(new Dweller('Anna', 'Kononenko', '26.10.1989', '099756474'));
+    .addDweller(new Dweller('Anna', 'Kononenko', '1989-10-26', '099756474'));
 
 greenville
     .getApartament(65)
-    .addDwellerInfo(new Dweller('Denys', 'Kononenko', '02.03.1987', '0997836474'));
+    .addDweller(new Dweller('Denys', 'Kononenko', '1999-09-03', '0997836474'));
 
 greenville
     .getApartament(54)
-    .addDwellerInfo(new Dweller('Maria', 'Maslova', '23.10.1996', '099756474'));
+    .addDweller(new Dweller('Maria', 'Maslova', '1975-03-02', '09973356474'));
 
 greenville
     .getApartament(202)
-    .addDwellerInfo(new Dweller('Anna', 'Voitenko', '02.03.1987', '099785474'));
+    .addDweller(new Dweller('Anna', 'Voitenko', '1975-03-02', '099785474'));
 
 greenville
     .getApartament(202)
-    .addDwellerInfo(new Dweller('Anton', 'Voitenko', '02.03.1987', '099785474'));
+    .addDweller(new Dweller('Anton', 'Voitenko', '1989-10-26', '099785474'));
 
 console.log(greenville);
+
+//існуюючі дані
 console.log(greenville.getApartament(54));
 console.log(greenville.getApartament(54).getDweellers());
+
+//неіснуюючі дані
+console.log(greenville.getApartament(303));
+console.log(greenville.getApartament(196).getDweellers());
