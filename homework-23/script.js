12a429617f3bb61fbb9127642018936408dd89ad
NataliSkidanenko <sni.nata102@gmail.com>
2022-11-08 10:50:28 +0200

    Changed path to images

--- homework-23/script.js
+++ homework-23/script.js
@@ -1,10 +1,10 @@
 const IMAGES = [
-    {src: 'dog1.jpeg', alt: 'dog1'},
-    {src: 'dog2.jpeg', alt: 'dog2'},
-    {src: 'dog3.jpeg', alt: 'dog3'},
-    {src: 'dog4.jpeg', alt: 'dog4'},
-    {src: 'dog5.jpeg', alt: 'dog5'},
-    {src: 'dog6.jpeg', alt: 'dog6'},
+    {src: 'img/dog1.jpeg', alt: 'dog1'},
+    {src: 'img/dog2.jpeg', alt: 'dog2'},
+    {src: 'img/dog3.jpeg', alt: 'dog3'},
+    {src: 'img/dog4.jpeg', alt: 'dog4'},
+    {src: 'img/dog5.jpeg', alt: 'dog5'},
+    {src: 'img/dog6.jpeg', alt: 'dog6'},
 ];
 
 function generateImg() {
@@ -15,5 +15,5 @@ function generateImg() {
 const img = document.querySelector('.img');
 const newImg = generateImg();
 
-img.setAttribute('src', `img/${newImg.src}`);
+img.setAttribute('src', newImg.src);
 img.setAttribute('alt', newImg.alt);
